194e7afe991807b15d05804aa5444ac2c8f11b45
Kayyleb <kaylebjuliancook@gmail.com>
2025-10-24 09:26:45 -0700

    Lab 2

---
+++ Lab2/SingletonLabStarterCode/SingletonDll/SingletonLogger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace SingletonDll
+{
+    public class SingletonLogger
+    {
+        private static SingletonLogger instance;
+        private static readonly object instanceLock = new object();
+        private StreamWriter streamWriter;
+        private object streamSync = new object();
+
+        private SingletonLogger(string logFile)
+        {
+            streamWriter = new StreamWriter(File.Create(logFile));
+        }
+        public static SingletonLogger GetInstance(string logFile)
+        {
+            if (instance == null)
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                        instance = new SingletonLogger(logFile);
+                }
+            }
+            return instance;
+        }
+
+        public void LogMsg(string msg)
+        {
+            lock (streamSync)
+            {
+                streamWriter.WriteLine("{0}  {1}", DateTime.Now, msg);
+                streamWriter.Flush();
+                Thread.Sleep(10);
+            }
+        }
+
+        public void Close()
+        {
+            streamWriter.Close();
+        }
+
+    }
+}
---
+++ Lab2/SingletonLabStarterCode/ThreadingDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using SingletonDll;
+using System.Diagnostics;
+
+
+namespace ThreadingDemo
+{
+    public class ExerciseThreads
+    {
+        
+        public static void ThreadLogger()
+        {
+            for (int nIter = 0; nIter < 100; nIter++)
+            {
+                SingletonLogger logger = SingletonLogger.GetInstance("log.txt");
+                logger.LogMsg(string.Format("Writing Message {0}", nIter));
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stop = Stopwatch.StartNew();
+
+            List<Thread> threadlist = new List<Thread>();
+
+            for (int nThread = 0; nThread < 5; nThread++)
+            {
+                threadlist.Add(new Thread(ExerciseThreads.ThreadLogger));
+                threadlist[nThread].Start();
+            }
+
+            foreach (Thread thread in threadlist)
+            {
+                thread.Join();
+            }
+
+            SingletonLogger.GetInstance("log.txt").Close();
+
+            stop.Stop();
+            Console.WriteLine($"Elapsed time: {stop.ElapsedMilliseconds} ms");
+        }
+
+    }
+}
